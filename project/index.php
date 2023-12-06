c53ac0ed3c97d8fe085e3329b40266f5f95771cd
Etudiant BD <ebd@localhost.localdomain>
2023-12-05 22:46:38 -0500

    Add content to accueil page

--- project/index.php
+++ project/index.php
@@ -1,1 +1,17 @@
-<?php
+<html>
+
+<head></head>
+
+<body>
+<?php include 'header.php'; ?>
+
+<label for="username">Username: </label>
+</input type="text" id="username" name="username"></br>
+<label for="password">Password: </label>
+</input type="password" id="password" name="password"></br>
+</input type="button" value="submit"></br>
+
+<?php include 'footer.php'; ?>
+</body>
+
+</html>
