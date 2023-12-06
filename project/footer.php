d7865766fc6ff09a0b1dbe28d82b67f0f48c6842
Etudiant BD <ebd@localhost.localdomain>
2023-12-06 00:56:45 -0500

    Ajout des fichiers php header et footer

---
+++ project/footer.php
@@ -0,0 +1,3 @@
+<?php
+date_default_timezone_set("America/Toronto");
+echo "<p>Tous droits réservés &copy; " . date("Y") . "Équipe 10 IFT-2004 Automne 2023</p>";
---
+++ project/header.php
@@ -0,0 +1,11 @@
+<?php
+?>
+<html>
+<head>
+
+</head>
+<body>
+<?php echo $erreur;?><br>
+	<p> <img src="<?php echo METTRENOMDUFICHIER;?>" width="500"></p>
+</body>
+</html>
