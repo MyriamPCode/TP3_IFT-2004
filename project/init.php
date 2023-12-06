5565f0ce738e28b2469885a6133ded771c63d23a
Etudiant BD <ebd@localhost.localdomain>
2023-12-06 11:48:43 -0500

    ajout init.php et creer barre etat php

--- project/init.php
+++ project/init.php
@@ -1,1 +1,20 @@
 <?php
+$conn = oci_connect('', '', 'fsg-p-ora01.fsg.ulaval.ca:1521/ora19c.fsg.ulaval.ca', 'AL32UTF8');
+$where = '';
+if(isset($_POST['Suivant']) ){
+    $where = " where NO_UTILISATEUR > ". $_POST['NO_UTILISATEUR'];
+}
+$stid = oci_parse($conn, 'select * from TP3_UTILISATEUR ' . $where .
+    ' fetch first 1 rows only');
+oci_execute($stid);
+$nomUtilisateur = "";
+$noUtilisateur = "";
+if(($utilisateur = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS))
+    != false){
+        $nomUtilisateur = $utilisateur["NOM_UTI"]; $erreur = "";
+        $noUtilisateur = $utilisateur["NO_UTILISATEUR"];
+}else{
+    $erreur = "Aucun utilisateur ne correspond à la requête";
+}
+oci_close($conn);
+?>
