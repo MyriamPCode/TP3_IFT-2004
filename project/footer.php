d8257a8c32473a5e5152d41da9c0110dc98a6514
Etudiant BD <ebd@localhost.localdomain>
2023-12-06 23:14:38 -0500

    création du php voir_question

--- project/footer.php
+++ project/footer.php
@@ -2,3 +2,4 @@
 date_default_timezone_set("America/Toronto");
 echo "<p>Tous droits réservés Mobili-T &copy; " . date("Y");
 echo "<p>Équipe 10 IFT-2004 Automne 2023</p>";
+?>
---
+++ project/voir_question.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+</head>
+
+<body>
+session_start()
+
+<?php include 'header.php'; ?>
+<?php include 'barre_etat.php'; ?>
+ $_SESSION
+
+
+<?php include 'footer.php'; ?>
+</body>
+
+</html>
