6cc516d3e9311c07cc8dce3d67d53137e5e6574e
Etudiant BD <ebd@localhost.localdomain>
2023-12-06 23:05:17 -0500

    Link login page to principal page

--- project/index.php
+++ project/index.php
@@ -1,18 +1,27 @@
 <!DOCTYPE html>
 <html>
 
-<head></head>
+<head>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css"/>
+</head>
 
 <body>
 <?php include 'header.php'; ?>
 
-<label for="username">Username: </label>
-<input type="text" id="username" name="username"></br>
-<label for="password">Password: </label>
-<input type="password" id="password" name="password"></br>
-<input type="button" value="submit"></br>
+<form method="post" action="principal.php">
+	<p>
+		<label for="username">Username: </label>
+    	<input type="text" id="username" name="username" />
+	</p>
+	<p>
+		<label for="password">Password: </label>
+    	<input type="password" id="password" name="password" />
+	</p>
+    <input type="submit" value="Submit" />
+</form>
 
 <?php include 'footer.php'; ?>
 </body>
 
 </html>
+
