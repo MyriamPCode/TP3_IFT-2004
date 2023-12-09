eda1c8af181199fbe293d772b6d81e3ab71e64b2
Etudiant BD <ebd@localhost.localdomain>
2023-12-09 18:16:29 -0500

    Add sondage archive dans une nouvelle table

--- project/principal.php
+++ project/principal.php
@@ -10,6 +10,7 @@
 <?php session_start(); ?>
 <?php include 'init.php' ?>
 
+<h2>Sondages en cours</h2>
 <table>
 	<tr>
 		<th>Numéro</th>
@@ -38,6 +39,40 @@
            
            echo "<td><a href='rapport.php'>Rapport</a></td>\n";
            echo "<td><a href='sondage_edit.php&NO_SONDAGE=<". $sondage['NO_SONDAGE'] .">''>Modifier</a></td>\n";
+           echo "</tr>\n";
+	   }
+	   
+	   closeDatabaseConnection();
+	?>
+</table>
+
+<h2>Sondages archivés</h2>
+<table>
+	<tr>
+		<th>Numéro</th>
+		<th>Projet</th>
+		<th>Titre</th>
+		<th>Date de début</th>
+		<th>Date de fin</th>
+		<th>Rapport</th>
+	</tr>
+	<?php 
+	   $selectSondages = "SELECT * FROM TP3_SONDAGE_ARCHIVE";
+	   $stid = performDatabaseQuery($selectSondages);
+	   $keys = array('NO_SONDAGE', 'CODE_PROJET', 'TITRE_SON', 'DATE_DEBUT_SON', 'DATE_FIN_SON');
+	   
+	   while (($sondage = oci_fetch_array($stid, OCI_ASSOC + OCI_RETURN_NULLS)) != false) {
+	       
+	       echo "<tr>\n";
+	       foreach ($keys as $item) {
+	           if($item  === 'NO_SONDAGE'){
+	               echo "<td><a href='liste_questions.php&NO_SONDAGE=<". $sondage[$item] .">'>" . $sondage[$item] . "</a></td>\n";
+	           } else {
+	               echo "<td>" . $sondage[$item] . "</td>\n";
+	           }
+           }
+           
+           echo "<td><a href='rapport.php'>Rapport</a></td>\n";
            echo "</tr>\n";
 	   }
 	   
