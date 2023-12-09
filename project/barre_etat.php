2dafddff919da3dee6e5acce34eb3bb44f53209b
Etudiant BD <ebd@localhost.localdomain>
2023-12-09 13:46:37 -0500

    Modification barre-etat

--- project/barre_etat.php
+++ project/barre_etat.php
@@ -1,1 +1,43 @@
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+</head>
+<body>
+
+<div id="menu">
+    <ul>
+        <!-- Lien de connexion -->
+        <li><a href="index.php">Connexion</a></li>
+    </ul>
+</div>
+
 <?php
+
+// Démarrez la session si ce n'est pas déjà fait
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+// Fonction pour vérifier si l'utilisateur est connecté
+function estConnecte() {
+    return isset($_SESSION['utilisateur']);
+}
+
+// Fonction pour afficher l'état de connexion
+function afficherEtatConnexion() {
+    if (estConnecte()) {
+        // Utilisateur connecté
+        $prenomNom = $_SESSION['utilisateur']['prenom'] . ' ' . $_SESSION['utilisateur']['nom'];
+        $typeUtilisateur = $_SESSION['utilisateur']['type'];
+        echo "Bienvenue $prenomNom ($typeUtilisateur) - <a href='deconnexion.php'>Déconnecter</a>";
+    } else {
+        // Utilisateur non connecté
+        echo "<a href='index.php'>Connexion</a>";
+    }
+}
+?>
+
+</body>
+</html>
