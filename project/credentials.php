b9ebc523227d20d21f774b54d582e97a0441d171
Etudiant BD <ebd@localhost.localdomain>
2023-12-10 15:02:42 -0500

    sondage_edit.php fini sauf barre_etat

--- project/credentials.php
+++ project/credentials.php
@@ -1,5 +1,5 @@
 <?php
-$username = "";
+$username = "C##";
 $password ="";
 ?>
 
--- project/voir_question.php
+++ project/voir_question.php
@@ -3,108 +3,144 @@ session_start();
 
 include 'init.php';
 
+//include 'barre_etat.php';
 
-$where = " where ID_QUESTION = ". $_POST['ID_QUESTION'];
+//estConnecte();
 
-$stid = oci_parse($conn, 'select * from TP3_QUESTION ' . $where . ' fetch first 1 rows only');
-oci_execute($stid);
 $noOrdreQuestion = "";
 $typeQuestion = "";
 $texteQuestion = "";
+$noSondage = "";
+$ordreReponse = "";
+$texteChoix = "";
 $titreSondage = "";
+$erreurSondage = "";
+$descTypeQue = "";
+$erreurReponse = "Aucun choix de réponse associé";
+$tableauChoixRep = "";
+
+if (isset($_GET['ID_QUESTION'])){
+
+// On trouve la question grâce à son ID provenant de la page sondage_edit.php
+$where = " where ID_QUESTION = ". $_GET['ID_QUESTION']; 
+
+$stid = oci_parse($conn, 'select * from TP3_QUESTION ' . $where . ' fetch first 1 rows only');
+oci_execute($stid);
 
 if(($question = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS))!= false){
     $noOrdreQuestion = $question["ORDRE_QUESTION"];
     $typeQuestion = $question["CODE_TYPE_QUESTION"];
     $texteQuestion = $question["TEXTE_QUE"];
-    $titreSondage = $question["NO_SONDAGE"];
+    $noSondage = $question["NO_SONDAGE"];
     $erreur = "";
+
+
+    // On trouve les choix de réponses associé au ID de la question
+
+    $tableauChoixRep = '<table>
+              <tr>
+                <th>Ordre</th>
+                <th>Texte</th>
+            </tr>';
+    
+    if ($typeQuestion == 'RB11')
+        {
+            $erreurReponse = "";
+            $texteChoix = "Question à développement";
+            $tableauChoixRep = '<table>
+                <tr>
+                    <td>'.$texteChoix.'</td>
+                  </tr>
+                </table>';
+            
+    }else {
+        $stid = oci_parse($conn, 'select * from TP3_CHOIX_REPONSE ' . $where . 'order by ORDRE_REPONSE');
+        oci_execute($stid);
+            while(($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) != false) {
+                $erreurReponse = "";
+                $ordreReponse = $row['ORDRE_REPONSE'];
+                $texteChoix = $row['TEXTE_CHO'];
+                $tableauChoixRep .= '<tr>
+                        <td>'.$ordreReponse.'</td>
+                        <td>'.$texteChoix.'</td>               
+                    </tr>';
+            }  
+        $tableauChoixRep .= '</table>';
+
+    }
+    
+    
+    // On trouve le titre du sondage
+    $where = " where NO_SONDAGE = ". $noSondage;
+    $stid = oci_parse($conn, 'select * from TP3_SONDAGE ' . $where . ' fetch first 1 rows only');
+    oci_execute($stid);
+    
+    if (($sondage = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS))!= false){
+        $titreSondage = $sondage["TITRE_SON"];
+    } else{
+        $erreurSondage = "Aucun sondage associé";
+    }
+    
+    
+    // On converti le code du type de question avec la description du type
+    $where = " where CODE_TYPE_QUESTION =  '".$typeQuestion."'";
+    
+    $stid = oci_parse($conn, 'select * from TP3_TYPE_QUESTION ' . $where . ' fetch first 1 rows only');
+    oci_execute($stid);
+    if(($typeQuestion = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS))!= false){
+        $descTypeQue = $typeQuestion["DESC_TYPE_QUE"];
+    }
+
+
 }else{
     $erreur = "Aucune question associé avec ce sondage";
 }
- 
-$stid = oci_parse($conn, 'select * from TP3_CHOIX_REPONSE ' . $where . 'order by ORDRE_REPONSE');
-oci_execute($stid);
-
-$ordreReponse = "";
-$texteChoix = "";
-$tableauChoixRep = '<table>
-          <tr>
-            <th>Ordre</th>
-            <th>Texte</th>
-          </tr>';
-
-if ($typeQuestion == 'RB11')
-    {
-        $ordreReponse = "";
-        $texteChoix = "Question à développement";
-        $tableauChoixRep = '<table>
-            <tr>
-                <td>'.$texteChoix.'</td>
-              </tr>';
-}else {
-    while(($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) != false) {    
-        $ordreReponse = $row['ORDRE_REPONSE'];
-        $texteChoix = $row['TEXTE_CHO'];
-        $tableauChoixRep .= '<tr>
-                <td>'.$ordreReponse.'</td>
-                <td>'.$texteChoix.'</td>               
-              </tr>';
-    }
 }
-$tableauChoixRep .= '</table>';
 
-
-$where = " where CODE_TYPE_QUESTION =  '".$typeQuestion."'";
-
-$stid = oci_parse($conn, 'select * from TP3_TYPE_QUESTION ' . $where . ' fetch first 1 rows only');
-oci_execute($stid);
-$descTypeQue = "";
-if(($typeQuestion = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS))!= false){
-    $descTypeQue = $typeQuestion["DESC_TYPE_QUE"];
-}
    
     
 oci_close($conn);
 ?>
+<!DOCTYPE html>
 
 <html>
 <head>
-<title>Détails de la question</title>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+	<title>Détails de la question</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
 </head>
 
 <body>
 <?php include 'header.php'; ?>
-<?php include 'barre_etat.php'; ?>
+ <?php //afficherEtatConnexion(); ?>
 
-<?php echo $erreur;?><br>
-<form method="post" action="voir_question.php">
-<p>
-Sondage: <input type="text" name="NO_SONDAGE" value="<?php echo $titreSondage;?>"><br>
-Question:
-<table>
-<tr>
-<th>Ordre</th>
-<th>Type</th>
-<th>Texte</th>
-</tr>
-<tr> 
-<td> <?php echo $noOrdreQuestion;?></td>
-<td> <?php echo $descTypeQue;?></td>
-<td> <?php echo $texteQuestion;?></td>
-</tr>
-</table>
+<!-- On affiche le tout -->
 
-Réponse:
-<?php echo $tableauChoixRep;?>
-    
+	Sondage:<br>
+	<label for="NO_SONDAGE"><?php echo $titreSondage;?></label><br><br>
+	<?php echo $erreurSondage;?>
+	Question: 
+	<?php echo $erreur;?><br>
+	<table>
+		<tr>
+			<th>Ordre</th>
+			<th>Type</th>
+			<th>Texte</th>
+		</tr>
+		<tr> 
+			<td> <?php echo $noOrdreQuestion;?></td>
+			<td> <?php echo $descTypeQue;?></td>
+			<td> <?php echo $texteQuestion;?></td>
+		</tr>
+	</table><br>
+
+	Réponse:
+	<?php echo $tableauChoixRep;?> 
+    <?php echo $erreurReponse;?><br><br>
+
+    <!-- Bouton contenant l'hyperlien pour revenir à la page sondage_edit -->
+	<input type="button" onclick="window.location.href='sondage_edit.php?NO_SONDAGE=<?php echo $noSondage;?>';" value="Retour au sondage"/>
 
 
-<input type="button" onclick="window.location.href='liste_sondage.php';" value="Retour aux sondages"/>
-
-</p>
-</form>
 
 <?php include 'footer.php'; ?>
 </body>
